ad7483c33fd9b37b6794c5da0c9ad8202bc22e02
Alexander Rothman <gnomesort@megate.ch>
2025-01-30 23:32:55 -0800

    Update Build Script and Add Examples
    This does three things:
    
    1. Changes meson.build to support example programs and extended
       configuration.
    2. Updates the configuration header in turn to add compiler ids.
    3. Adds an initialization example program.

---
+++ examples/initialization.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#include <megatech/vulkan.hpp>
+#include <megatech/vulkan/adaptors/libvulkan.hpp>
+
+namespace mv = megatech::vulkan;
+namespace mval = megatech::vulkan::adaptors::libvulkan;
+
+template <typename OutputStream>
+OutputStream& insert_severity(OutputStream&& out, const mv::bitmask severity) {
+  out << "[";
+  switch (severity)
+  {
+  case mv::debug_message_severity::error_bit:
+    out << "Error";
+    break;
+  case mv::debug_message_severity::warning_bit:
+    out << "Warning";
+    break;
+  case mv::debug_message_severity::info_bit:
+    out << "Info";
+    break;
+  case mv::debug_message_severity::verbose_bit:
+    out << "Verbose";
+    break;
+  }
+  return out << "]: ";
+}
+
+void debug_sink(const mv::bitmask, const mv::bitmask severity, const std::string& message) {
+  insert_severity(std::clog, severity) << message << "\n";
+}
+
+int main() {
+  auto debugger = mv::debug_messenger_description{ debug_sink };
+  auto inst = mval::debug_instance{ mval::loader{ },  { }, { "VK_LAYER_KHRONOS_validation" }, debugger };
+  auto physical_devices = mv::physical_device_list{ inst };
+  if (physical_devices.empty())
+  {
+    return 1;
+  }
+  auto dev = mval::device{ physical_devices.front() };
+  return 0;
+}
